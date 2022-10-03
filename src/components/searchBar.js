efde288ae2e1ea34ccfd6aa777e7a831d75d4981
tlfinn1618 <tlfinn1618@gmail.com>
2022-10-02 21:09:12 -0700

    added handleSubmit functionality to searchBar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class SearchBar extends Component {
-  render() {
+  handleFormSubmit = function ({ query }) {
+    console.log("trying to handle submit for query", query);
+  };
+
+  renderInput(field) {
     return (
-      <div>
-        <input placeholder="Search Daily Smarty" />
-      </div>
+      <input
+        type="text"
+        placeholder="Search DailySmarty"
+        {...field.input}
+      />
+    );
+  }
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form
+        className="search-bar"
+        onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}
+      >
+        <Field
+          name="query"
+          component={this.renderInput}
+        />
+      </form>
     );
   }
 }
+
+SearchBar = reduxForm({
+  form: "searchBar"
+})(SearchBar);
 
 export default SearchBar;
