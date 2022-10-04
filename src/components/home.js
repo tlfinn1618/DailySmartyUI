990c952bc148ab6c9ea4ab09b524a64be621596d
tlfinn1618 <tlfinn1618@gmail.com>
2022-10-03 20:03:56 -0700

    Setup app routes

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ import Logo from "./logo";
 import RecentPosts from "./recentPosts";
 import SearchBar from "./searchBar";
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
       <div className="app">
