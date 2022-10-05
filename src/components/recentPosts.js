6b20df5cbc5256b290eefbd82221ddf0b70cc9be
tlfinn1618 <tlfinn1618@gmail.com>
2022-10-04 21:20:42 -0700

    added mapStateToProps in the recentPosts and displayed the top 3 with li tags

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -9,20 +9,31 @@ class RecentPosts extends Component {
     this.props.fetchRecentPosts();
   }
 
+  renderPosts = function () {
+    const posts = this.props.recentPosts.map((post, index) => {
+      if (index < 3) {
+        return <li key={index}>{post.title}</li>;
+      }
+    });
+    return posts;
+  };
+
   render() {
     return (
       <div className="recent-posts">
         <div className="recent-posts_wrapper">
           <div className="recent-posts_heading">Recent Posts</div>
-          <ul className="recent-posts_posts">
-            <li>recent post 0</li>
-            <li>recent post 1</li>
-            <li>recent post 2</li>
-          </ul>
+          <ul className="recent-posts_posts">{this.renderPosts()}</ul>
         </div>
       </div>
     );
   }
 }
 
-export default connect(null, actions)(RecentPosts);
+function mapStateToProps(state) {
+  return {
+    recentPosts: state.posts.recentPosts
+  };
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
