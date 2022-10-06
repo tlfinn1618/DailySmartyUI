f6f8965bbef888c13b2f672bb287f24b25ff9357
tlfinn1618 <tlfinn1618@gmail.com>
2022-10-06 11:30:21 -0700

    Mapping state to props in a results post component and added Link to the logo to Home

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Logo extends Component {
   render() {
@@ -9,11 +10,13 @@ class Logo extends Component {
 
     return (
       <div className="logo-main">
-        <img
-          style={size}
-          alt="Logo Image"
-          src="/assets/ds_circle_logo.png"
-        />
+        <Link to="/">
+          <img
+            style={size}
+            alt="Logo Image"
+            src="/assets/ds_circle_logo.png"
+          />
+        </Link>
       </div>
     );
   }
--- src/components/resultsPosts.js
+++ src/components/resultsPosts.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
 
 import { connect } from "react-redux";
+import Post from "./post";
 
 class ResultsPosts extends Component {
+  renderPosts() {
+    const posts = this.props.posts.map((post, index) => {
+      return (
+        <Post
+          key={index}
+          {...post}
+        />
+      );
+    });
+    return posts;
+  }
+
   render() {
     return (
       <div className="results-posts">
         <div className="results-posts_wrapper">
-          <ul className="results-posts_posts">This worked</ul>
+          <ul className="results-posts_posts">{this.renderPosts()}</ul>
         </div>
       </div>
     );
@@ -16,7 +29,7 @@ class ResultsPosts extends Component {
 
 function mapStateToProps(state) {
   return {
-    state
+    posts: state.posts.resultsPosts
   };
 }
 
