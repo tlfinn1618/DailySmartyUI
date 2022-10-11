78a6b21ce3d6d861ea3adf5c2d89d218c8fa7130
tlfinn1618 <tlfinn1618@gmail.com>
2022-10-10 20:37:33 -0700

    finished styling components

--- src/components/post.js
+++ src/components/post.js
@@ -52,17 +52,15 @@ class Post extends Component {
       );
     } else if (this.props.type == "result") {
       return (
-        <li className="result-post">
+        <li
+          className="result-post"
+          onMouseEnter={() => this.setState({ height: 70 })}
+          onMouseLeave={() => this.setState({ height: 0 })}
+        >
           <div className="result-post_topic">{this.renderTopics()}</div>
 
           <div className="result-post_title">
-            <a
-              href={this.props.url_for_post}
-              onMouseEnter={() => this.setState({ height: 70 })}
-              onMouseLeave={() => this.setState({ height: 0 })}
-            >
-              {this.props.title}
-            </a>
+            <a href={this.props.url_for_post}>{this.props.title}</a>
           </div>
 
           <AnimateHeight
